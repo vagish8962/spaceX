597fb24b7291dab696d460b8959d002c0e1fd92c
vagish8962 <72687724+vagish8962@users.noreply.github.com>
2020-10-16 10:57:29 +0530

    Update UI.js

--- src/UI.js
+++ src/UI.js
@@ -4,18 +4,31 @@ function UI () {
     this.showSpaceX = (spaceX) => {
         this.profile.innerHTML = '';
         if (spaceX.spaceXdata.length > 0) {
-            spaceX.spaceXdata.map( spaceX =>  {
-                const str = ` <div class="col-sx-3 col">
-                <div class="pic">
-                    <img src="${spaceX.links.mission_patch_small}" alt="">
-                </div>
-                <h3>${spaceX.rocket.rocket_name}</h3>
-                <p>Mission IDS: <span> ${spaceX.mission_id[0]}</span></p>
-                <p>Launch Year: <span>${spaceX.launch_year}</span></p>
-                <p>Successful Launch: <span>${spaceX.launch_success}</span></p>
-                <p>Successful Landing: <span>${spaceX.rocket.first_stage.cores[0].land_success}</span></p>
-    
-            </div>`;
+            spaceX.spaceXdata.forEach( spaceX =>  {
+                let landtag = document.createElement('p');
+                let launchFlag = spaceX.rocket.first_stage.cores[0].land_success !== null ? 
+                    `<p>Successful Landing: <span>${spaceX.rocket.first_stage.cores[0].land_success}</span></p>`
+                    : 
+                    '';
+                landtag.insertAdjacentHTML( 'beforeend', `${launchFlag}` ); 
+              
+                let missions = document.createElement('ul');
+                spaceX.mission_id.forEach((misson) => {
+                    missions.insertAdjacentHTML( 'beforeend', `<li>${misson}</li>` ); 
+                });
+            const str = ` <div class="col-sx-3 col col-sx-md-6">
+                    <article class="space-each">
+                    <div class="pic">
+                        <img src="${spaceX.links.mission_patch_small}" alt="${spaceX.rocket.rocket_name}">
+                    </div>
+                    <h3>${spaceX.rocket.rocket_name}</h3>
+                    <p>Mission IDS:<ul> ${missions.innerHTML}</ul></p>
+                    <p>Launch Year: <span>${spaceX.launch_year}</span></p>
+                    <p>Successful Launch: <span>${spaceX.launch_success}</span></p>
+                    ${landtag.innerHTML}
+                    </article>
+                </div>`;
+                console.log(str);
                 this.profile.insertAdjacentHTML('beforeend', str);
             });
         } else {
