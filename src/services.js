105fc5ef85c790c7e30d5eeba9b4e8b269e9d7ff
vagish8962 <72687724+vagish8962@users.noreply.github.com>
2020-10-16 12:58:02 +0530

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,11 +1,12 @@
 function Services () {
     this.getData = async (filter, t) => {
         const queryStrings = '?limit=100&' + new URLSearchParams(filter).toString();
-        const url = (t == 'load' && localStorage.getItem('url'))? localStorage.getItem('url') : `https://api.spaceXdata.com/v3/launches${queryStrings}`;
+        const url = (t == 'load' && localStorage.getItem('spaceurl'))? localStorage.getItem('spaceurl') : `https://api.spaceXdata.com/v3/launches${queryStrings}`;
+        // url = url.split("?") ; 
         const spaceXResponse = await fetch(url);
 
         const spaceXdata = await spaceXResponse.json();
-        localStorage.setItem('url', url)
+        localStorage.setItem('surl', url);
         return {
             spaceXdata
         }
